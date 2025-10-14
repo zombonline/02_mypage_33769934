c6954875503d27b60af6050fb3b0f28f749975db
Bradley's Laptop <megazombie130@gmail.com>
2025-10-14 11:26:01 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require("http");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Hello World!</h1>\n<p>This is my first Node.js web application and it is now being hosted on a virtual machine!</p>");
     }).listen(port, function() { 
         console.log(`Node server is now running on port ${port}...`); 
 }); 
