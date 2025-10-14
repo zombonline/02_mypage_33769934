02d7a41038e299db330834a513484c35e474c8ff
Bradley's Laptop <megazombie130@gmail.com>
2025-10-14 12:41:06 +0100

    Updates to html and server response logic
    updated index.js to respond to requests for files such as images etc.
    moved html to its own file
    added a cute picture of a cat on the page!

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MyPage
 A very small node.js web application.
+The repo is currently comprised of two branches, main which is what the virtual machine will host, and dev which will be used for development and only merged into main when stable and ready to go!
--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 var http = require("http"); 
+var fs = require("fs");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Hello World!</h1>\n<p>This is my first Node.js web application and it is now being hosted on a virtual machine!</p>");
+    console.log("Request for " + req.url + " received.");
+    if (req.url === "/"){
+        res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("index.html", function(err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.end("404 Not Found");
+        } else {
+            res.end(data);
+        }
+    }); 
+    } else {
+        sendFileContent(res, req.url.substr(1), "text/html");
+    }
     }).listen(port, function() { 
         console.log(`Node server is now running on port ${port}...`); 
 }); 
+
+function sendFileContent(res, fileName, contentType){
+    fs.readFile(fileName, function(err, data){
+        if(err){
+            res.writeHead(404);
+            res.end("404 Not Found");
+        } else {
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(data);
+        }
+    });
+}
